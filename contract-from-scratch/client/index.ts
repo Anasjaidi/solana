cdfdfbe19ce4f2fdbe449f39a709a1e7c11d10fc
Anas jaidi <anas.jaidi@icloud.com>
2022-11-05 14:02:48 +0100

    chore: create client ts structure

---
+++ contract-from-scratch/client/index.ts
@@ -0,0 +1,15 @@
+const web3 = require('@solana/web3.js');
+
+async function main() {
+
+}
+
+
+main()
+  .then(
+    () => process.exit(0)
+  )
+  .catch(e => {
+    console.error(e);
+    process.exit(1)
+  })
