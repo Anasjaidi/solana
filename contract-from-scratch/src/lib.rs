9f30d3113eeb707a94fb61407af9bd885b2ac1a3
Anas jaidi <anas.jaidi@icloud.com>
2022-11-05 13:38:13 +0100

    chore: add small instruction logic
    printing instruction associated data

--- contract-from-scratch/src/lib.rs
+++ contract-from-scratch/src/lib.rs
@@ -3,16 +3,25 @@ use solana_program::{
 };
 
 entrypoint!(process_instruction);
+
 fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
-    instruction_data: &[u8],
+    instructions: &[u8],
 ) -> ProgramResult {
+    let key = instructions.first().unwrap();
+    match key {
+        0 => msg!("zero"),
+        1 => msg!("one"),
+        _ => msg!("panic"),
+    }
     msg!(
-        "process_instruction: {}: {} accounts, data={:?}",
+        "program_id {} \n
+        accounts {}\n
+        data {:?}",
         program_id,
         accounts.len(),
-        instruction_data
+        instructions
     );
     Ok(())
 }
